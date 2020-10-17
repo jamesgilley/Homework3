a69091456478dcfbcbbc09a42e809be1e979fd88
jamesgilley <70067688+jamesgilley@users.noreply.github.com>
2020-10-17 07:55:30 -0700

    udates to javasript code and readme.md

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
 # Homework3
+
+ In completing this assignment, I was expected to create a random password generator. This needed to include options of special characters, upper case characters, and lower case characters.  
+
+
+
+### Installing
+
+Use Your Browser to view work.
+
+If you click on the link provided, the file should open in your default browser.
+
+## Built With
+
+* HTML
+* CSS
+* Bootstrap
+* JavaScript
+* GoogleFoo
+
+## Authors
+
+* **James Gilley**
+
+- [Link to Portfolio Site](https://jamesgilley.github.io/homework2/)
+- [Link to Github](https://github.com/jamesgilley/Homework3)
+- [Link to LinkedIn](https://www.linkedin.com/in/james-gilley-312466187/)
+
+
+
+
+## Acknowledgments
+
+* Thank you to those grading this assignment. All criticism is welcomed and appreciated. 
+[Preview Link ](  https://jamesgilley.github.io/homework2/)
--- script.js
+++ script.js
@@ -1,38 +1,41 @@
-var specialChar = [" ","!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\", "]", "^", "_", "`", "{", "|", "}", "~", '"']
-var numChar = ["0", "2", "3", "4", "5", "6", "7", "8", "9"]
-var lowerCharCase = ["a", "b"]
-var upperCharCase = ["A", "B"]
-
-function passwordOptions() {
-  var charlength = parseInt(prompt("Please enter a password with 8 Charaters or more, no more than 128."))
- 
-}
-//conditionals if the user gave input
-//conditional if the password is less than 8
-//conditional if the password is more than 128
-//var with confirms click okay for special characters
-//var with confirms click okay for special integers
-//var with confirms click okay for lowercase
-//var with confirms click okay for upper case
-//
-
-// function randomNumber(){
-//   var randomIndex = Math.floor(math.random() * .length)
-// }
-
-
+var specialChars = [" ","!", "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".", "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", "}", "~", '"']
+var numChars = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+var lowerCharCase = 'abcdefghijklmnopqrstuvwxyz';
+var upperCharCase = lowerCharCase.toUpperCase();
 
 
 // // Assignment Code
-// var generateBtn = document.querySelector("#generate");
+var generateBtn = document.querySelector("#generate");
 // // Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
+function writePassword() {
+  var lowerCase = document.getElementById('lowerCase').checked;
+  var upperCase = document.getElementById('upperCase').checked;
+  var numeric = document.getElementById('numeric').checked;
+  var special = document.getElementById('special').checked;
+  var characterTypes = [lowerCase, upperCase, numeric, special];
+  var isOneChecked = characterTypes.some(type => type);
+  if (isOneChecked) {
+    let charLength;
+    do {
+      charLength = prompt("Please enter the passesword length, between 8 and 128 characters.");
+    } while (charLength === '' || parseInt(charLength) < 8 || parseInt(charLength) > 128)
 
-//   passwordText.value = password;
-
-// }
+    charLength = parseInt(charLength);
+    if (charLength !== null) {
+      let password = "";
+      for (let i = 0; i < charLength; i++) {
+        if (lowerCase && password.length < charLength) password += lowerCharCase[Math.floor(Math.random() * lowerCharCase.length)];
+        if (upperCase && password.length < charLength) password += upperCharCase[Math.floor(Math.random() * upperCharCase.length)];
+        if (numeric && password.length < charLength) password += numChars[Math.floor(Math.random() * numChars.length)];
+        if (special && password.length < charLength) password += specialChars[Math.floor(Math.random() * specialChars.length)];
+      }
+      document.querySelector("#password").value = `Your Secure Password is: ${password}`;
+    }
+  } else {
+    alert('At least one of the character types must be selected');
+  }
+}
 
 // // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
+
